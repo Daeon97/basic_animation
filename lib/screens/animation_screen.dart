ddb48fc372e8a7e7372406ec6598017646b177f4
innov8-engels <engelmmanuel@gmail.com>
2022-11-02 14:49:55 +0100

    Added an animation to animation screen

--- lib/screens/animation_screen.dart
+++ lib/screens/animation_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class AnimationScreen extends StatefulWidget {
   const AnimationScreen({Key? key}) : super(key: key);
@@ -7,12 +8,68 @@ class AnimationScreen extends StatefulWidget {
   State<AnimationScreen> createState() => _AnimationScreenState();
 }
 
-class _AnimationScreenState extends State<AnimationScreen> {
+class _AnimationScreenState extends State<AnimationScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animController;
+  late CurvedAnimation curvedAnimation;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    animController = AnimationController(
+      duration: const Duration(
+        seconds: 5,
+      ),
+      vsync: this,
+    );
+
+    curvedAnimation = CurvedAnimation(
+      parent: animController,
+      curve: Curves.bounceIn,
+      reverseCurve: Curves.easeOut,
+    );
+
+    animation = Tween<double>(
+      begin: 0,
+      end: 2 * math.pi,
+    ).animate(
+      curvedAnimation,
+    )
+      ..addListener(() {
+        setState(
+          () {},
+        );
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          animController.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          animController.forward();
+        }
+      });
+
+    animController.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: Center(
-          child: Image.asset(
-            'assets/resocoder.png',
+        body: Transform.rotate(
+          angle: animation.value,
+          child: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(
+              30.0,
+            ),
+            child: Image.asset(
+              'assets/resocoder.png',
+            ),
           ),
         ),
       );
