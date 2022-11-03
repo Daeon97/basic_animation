bcffa4ead2a325d6b17ce6b80bafdd8dc3b05bbe
innov8-engels <engelmmanuel@gmail.com>
2022-11-03 11:42:00 +0100

    Used AnimatedBuilder to implement animation

--- lib/screens/animation_screen.dart
+++ lib/screens/animation_screen.dart
@@ -1,4 +1,3 @@
-import 'dart:math' as math;
 import 'package:flutter/material.dart';
 
 class AnimationScreen extends StatefulWidget {
@@ -18,7 +17,7 @@ class _AnimationScreenState extends State<AnimationScreen>
   void initState() {
     animationController = AnimationController(
       duration: const Duration(
-        seconds: 10,
+        seconds: 5,
       ),
       vsync: this,
     );
@@ -31,63 +30,46 @@ class _AnimationScreenState extends State<AnimationScreen>
 
     animation = Tween<double>(
       begin: 0,
-      end: 2 * math.pi,
+      end: 1,
     ).animate(
       curvedAnimation,
     )
-      ..addListener(() {
-        setState(() {});
-      })
       ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          animationController.reverse();
-        } else if (status == AnimationStatus.dismissed) {
+        if (status == AnimationStatus.dismissed) {
           animationController.forward();
+        } else if (status == AnimationStatus.completed) {
+          animationController.reverse();
         }
       });
 
     animationController.forward();
-
     super.initState();
   }
 
   @override
   void dispose() {
     animationController.dispose();
+
     super.dispose();
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: Center(
-          child: Transform.rotate(
-            angle: animation.value,
-            child: Container(
-              alignment: Alignment.center,
-              width: 200,
-              height: 200,
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                  colors: [
-                    Colors.blue,
-                    Colors.green,
-                    Colors.yellow,
-                  ],
-                ),
-                borderRadius: BorderRadius.circular(
-                  16.0,
-                ),
-              ),
-              child: const Text(
-                'I Love Flutter :-)',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30.0,
-                ),
-              ),
-            ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnimatedBuilder(
+        animation: animation,
+        child: Center(
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.red,
           ),
         ),
-      );
+        builder: (context, child) => SizeTransition(
+          sizeFactor: animation,
+          child: child,
+        ),
+      ),
+    );
+  }
 }
